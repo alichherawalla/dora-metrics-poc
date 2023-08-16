4d0c2f22af9cb4d6ec503300eaea0e15fedadf8b
Abhishek Mishra <abhishek@wednesday.is>
2023-08-16 19:55:34 +0530

    chore: test ci

--- automation.py
+++ automation.py
@@ -2,7 +2,6 @@ import subprocess
 import re
 import os
 import datetime
-import yaml
 
 # Run git log command to get merge commit messages
 git_log_command = "git log --oneline origin/main..origin/dev"
@@ -26,36 +25,20 @@ for message in merge_commits:
     elif re.search(hotfix_pattern, message):
         hotfix_messages.append(message)
 
-# Path to the target YAML file
-target_yaml_file = "metrics.yaml"
-
-# Check if the metrics file exists
-if os.path.exists(target_yaml_file):
-    # Read the existing YAML content
-    with open(target_yaml_file, "r") as yaml_file:
-        existing_yaml = yaml.safe_load(yaml_file)
-    
-    # Determine the number of releases from the existing YAML content
-    total_releases = existing_yaml.get("total_releases", 0)
-    
-    # Generate the YAML content for releases
-    release_yaml = ""
-    for release_number in range(1, total_releases + 1):
-        release_info = existing_yaml.get(f"release_{release_number}", {})
-        release_yaml += f"""
+# Generate the YAML content for releases
+release_yaml = ""
+for release_number in range(1, 4):  # Assuming three releases
+    release_yaml += f"""
 release_{release_number}:
-  bugs: {release_info.get("bugs", 0)}
-  features: {release_info.get("features", 0)}
-  hotfixes: {release_info.get("hotfixes", 0)}
+  bugs: {release_number * 2}
+  features: {release_number * 10}
+  hotfixes: {release_number}
 """
-else:
-    total_releases = 0
-    release_yaml = ""
 
 # Generate the overall YAML content
 yaml_content = f"""
 cfr: 0
-total_releases: {total_releases}
+total_releases: 3  # Update this number based on the actual number of releases
 total_features: {len(feature_messages)}
 total_bugs: {len(bug_messages)}
 total_hotfixes: {len(hotfix_messages)}
@@ -63,9 +46,19 @@ total_hotfixes: {len(hotfix_messages)}
 {release_yaml}
 """
 
-# Write the updated YAML content
-with open(target_yaml_file, "w") as file:
+# Path to the target YAML file
+target_yaml_file = "metrics.yaml"
+
+# Write the YAML content to a temporary file
+temp_yaml_file = "temp_metrics.yaml"
+with open(temp_yaml_file, "w") as file:
     file.write(yaml_content)
+
+# Use yq to update the target YAML file in place
+subprocess.run(["yq", "eval", "-i", f". = import('{temp_yaml_file}')", target_yaml_file])
+
+# Remove the temporary file
+os.remove(temp_yaml_file)
 
 # Commit the changes to the current branch
 subprocess.run(["git", "add", target_yaml_file])
